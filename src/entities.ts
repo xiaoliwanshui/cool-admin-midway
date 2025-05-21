db8d6634e517d6d04ea48d5fc3bfdcbf62ce9f83
17691002584 <17691002584@163.com>
2025-05-21 11:59:41 +0800

    feat(video): 增加视频采集功能并优化视频相关服务- 新增 TaskCollectService 服务实现日常和周常采集功能- 扩展 CollectionService，增加 day 和 week 方法支持定期采集- 优化 VideoBean 类，添加备注和资源信息相关属性及方法 - 更新视频相关控制器和视图，增加采集状态过滤和显示 - 新增 ViewsService 服务处理用户浏览记录
    Signed-off-by: 17691002584 <17691002584@163.com>

--- src/entities.ts
+++ src/entities.ts
@@ -1,2 +1,115 @@
 // 自动生成的文件，请勿手动修改
-export const entities = [];
+import * as entity0 from './modules/video/entity/week_video';
+import * as entity1 from './modules/video/entity/week';
+import * as entity2 from './modules/video/entity/video_line';
+import * as entity3 from './modules/video/entity/videos';
+import * as entity4 from './modules/video/entity/swiper';
+import * as entity5 from './modules/video/entity/play_line';
+import * as entity6 from './modules/video/entity/player';
+import * as entity7 from './modules/video/entity/live';
+import * as entity8 from './modules/video/entity/collection_task';
+import * as entity9 from './modules/video/entity/collection_category';
+import * as entity10 from './modules/video/entity/collection';
+import * as entity11 from './modules/video/entity/category';
+import * as entity12 from './modules/video/entity/barrage';
+import * as entity13 from './modules/video/entity/album_video';
+import * as entity14 from './modules/video/entity/album';
+import * as entity15 from './modules/video/entity/ads';
+import * as entity16 from './modules/user/entity/wx';
+import * as entity17 from './modules/user/entity/views';
+import * as entity18 from './modules/user/entity/share';
+import * as entity19 from './modules/user/entity/like';
+import * as entity20 from './modules/user/entity/info';
+import * as entity21 from './modules/user/entity/download';
+import * as entity22 from './modules/user/entity/collect';
+import * as entity23 from './modules/user/entity/address';
+import * as entity24 from './modules/task/entity/log';
+import * as entity25 from './modules/task/entity/info';
+import * as entity26 from './modules/space/entity/type';
+import * as entity27 from './modules/space/entity/info';
+import * as entity28 from './modules/recycle/entity/data';
+import * as entity29 from './modules/plugin/entity/info';
+import * as entity30 from './modules/dict/entity/type';
+import * as entity31 from './modules/dict/entity/info';
+import * as entity32 from './modules/demo/entity/goods';
+import * as entity33 from './modules/comic/entity/swiper';
+import * as entity34 from './modules/comic/entity/image_comic';
+import * as entity35 from './modules/comic/entity/comic';
+import * as entity36 from './modules/comic/entity/album_comic';
+import * as entity37 from './modules/comic/entity/album';
+import * as entity38 from './modules/cloudDisk/entity/swiper';
+import * as entity39 from './modules/cloudDisk/entity/cloudDisk';
+import * as entity40 from './modules/cloudDisk/entity/album_cloudDisk';
+import * as entity41 from './modules/cloudDisk/entity/album';
+import * as entity42 from './modules/base/entity/base';
+import * as entity43 from './modules/base/entity/sys/user_role';
+import * as entity44 from './modules/base/entity/sys/user';
+import * as entity45 from './modules/base/entity/sys/role_menu';
+import * as entity46 from './modules/base/entity/sys/role_department';
+import * as entity47 from './modules/base/entity/sys/role';
+import * as entity48 from './modules/base/entity/sys/param';
+import * as entity49 from './modules/base/entity/sys/menu';
+import * as entity50 from './modules/base/entity/sys/log';
+import * as entity51 from './modules/base/entity/sys/department';
+import * as entity52 from './modules/base/entity/sys/conf';
+import * as entity53 from './modules/application/entity/noticeInfo';
+import * as entity54 from './modules/application/entity/appMenu';
+import * as entity55 from './modules/application/entity/appList';
+export const entities = [
+  ...Object.values(entity0),
+  ...Object.values(entity1),
+  ...Object.values(entity2),
+  ...Object.values(entity3),
+  ...Object.values(entity4),
+  ...Object.values(entity5),
+  ...Object.values(entity6),
+  ...Object.values(entity7),
+  ...Object.values(entity8),
+  ...Object.values(entity9),
+  ...Object.values(entity10),
+  ...Object.values(entity11),
+  ...Object.values(entity12),
+  ...Object.values(entity13),
+  ...Object.values(entity14),
+  ...Object.values(entity15),
+  ...Object.values(entity16),
+  ...Object.values(entity17),
+  ...Object.values(entity18),
+  ...Object.values(entity19),
+  ...Object.values(entity20),
+  ...Object.values(entity21),
+  ...Object.values(entity22),
+  ...Object.values(entity23),
+  ...Object.values(entity24),
+  ...Object.values(entity25),
+  ...Object.values(entity26),
+  ...Object.values(entity27),
+  ...Object.values(entity28),
+  ...Object.values(entity29),
+  ...Object.values(entity30),
+  ...Object.values(entity31),
+  ...Object.values(entity32),
+  ...Object.values(entity33),
+  ...Object.values(entity34),
+  ...Object.values(entity35),
+  ...Object.values(entity36),
+  ...Object.values(entity37),
+  ...Object.values(entity38),
+  ...Object.values(entity39),
+  ...Object.values(entity40),
+  ...Object.values(entity41),
+  ...Object.values(entity42),
+  ...Object.values(entity43),
+  ...Object.values(entity44),
+  ...Object.values(entity45),
+  ...Object.values(entity46),
+  ...Object.values(entity47),
+  ...Object.values(entity48),
+  ...Object.values(entity49),
+  ...Object.values(entity50),
+  ...Object.values(entity51),
+  ...Object.values(entity52),
+  ...Object.values(entity53),
+  ...Object.values(entity54),
+  ...Object.values(entity55),
+];
