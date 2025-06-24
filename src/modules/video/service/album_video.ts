392faa2b54cd1c04472b396bca25424b7769cf8f
17691002584 <17691002584@163.com>
2025-06-24 23:04:54 +0800

    feat(video): 优化视频专辑功能并添加新接口
    - 在专辑列表中添加关键字搜索功能
    - 新增专辑视频列表接口
    - 优化视频排序查询逻辑
    - 修复视频实体插入时的唯一键冲突问题

--- src/modules/video/service/album_video.ts
+++ src/modules/video/service/album_video.ts
@@ -1,8 +1,9 @@
-import { Provide} from "@midwayjs/core";
+import {ILogger, Inject, Provide} from "@midwayjs/core";
 import {InjectEntityModel} from "@midwayjs/typeorm";
 import {VideoEntity} from "../entity/videos";
 import {In, Repository} from "typeorm";
 import { VideoAlbumRelationship} from "../entity/video_album_relationship";
+import {VideoAlbumEntity} from "../entity/album";
 
 const TAG = 'AlbumVideoServer';
 
@@ -13,6 +14,12 @@ export class AlbumVideoServer {
 
   @InjectEntityModel(VideoAlbumRelationship)
   videoAlbumRelationship: Repository<VideoAlbumRelationship>;
+
+  @InjectEntityModel(VideoAlbumEntity)
+  albumEntity: Repository<VideoAlbumEntity>;
+
+  @Inject()
+  logger: ILogger;
 
   //批量添加专辑内容
   async insertAlbumVideo(id:number,title:[string]): Promise<any> {
@@ -25,5 +32,71 @@ export class AlbumVideoServer {
         };
       })
     );
+  }
+
+  async album(query: any): Promise<any> {
+    let { list } = await this.videoAlbumEntityPage(query);
+    return {
+      list,
+      pagination:this.setPageDefault(query)
+    }
+  }
+
+  setPageDefault(query: any): any {
+    query.page = query.page ? query.page : 1;
+    query.size = query.size ? query.size : 10;
+    query.videoSize = query.videoSize ? query.videoSize : 4;
+    query.videoPage = query.videoPage ? query.videoPage : 1;
+    return query;
+  }
+
+
+
+
+
+  //查询albumEntity分页
+  async videoAlbumEntityPage(query: any): Promise<any> {
+    //给分页设置默认值
+    query = this.setPageDefault(query);
+    const data:VideoAlbumEntity[] = await this.albumEntity.find(
+      {
+        where: {
+          category_id: query.category_id
+        },
+        order: {
+          sort: 'ASC',
+        },
+        skip: query.page * (query.page - 1),
+        take: query.size,
+      }
+    )
+    if(!data.length){
+     return {list:data}
+    }
+   return  this.videoAlbumRelationshipPage(data,query)
+  }
+
+  async videoAlbumRelationshipPage(data: Array<any>, query: any): Promise<any> {
+   if(data.length){
+     for (const item of data) {
+       let video = [];
+       let data = await this.videoAlbumRelationship.find({
+         where: { album_id: item.id },
+         skip: query.videoPage * (query.videoPage - 1),
+         take: query.videoSize,
+       });
+       for (const dataItem of data) {
+         video.push(
+           await this.videoEntity.findOneBy({
+             id: dataItem.videos_id,
+           })
+         );
+       }
+       item['list'] = video;
+     }
+   }else {
+     data = []
+   }
+    return {list:data}
   }
 }
