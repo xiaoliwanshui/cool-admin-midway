020033d55bdf58bc32849b084964ab3327b4bb0f
17691002584 <17691002584@163.com>
2025-06-24 21:51:47 +0800

    fix(config): 修改数据库密码
    - 将 config.prod.ts 文件中的数据库密码从 'nXAbTjbDm4sMwdbd' 修改为 'n4mM4KHkEyaNZEm2'
    - 此修改提高了数据库的安全性

--- src/config/config.prod.ts
+++ src/config/config.prod.ts
@@ -15,7 +15,7 @@ export default {
         host: '127.0.0.1',
         port: 3306,
         username: 'cms',
-        password: 'nXAbTjbDm4sMwdbd',
+        password: 'n4mM4KHkEyaNZEm2',
         database: 'cms',
         // 自动建表 注意：线上部署的时候不要使用，有可能导致数据丢失
         synchronize: false,
