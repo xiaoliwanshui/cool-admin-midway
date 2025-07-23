e485ba037698830927ab5c766cfdf9d6025b2d04
17691002584 <17691002584@163.com>
2025-07-23 19:51:34 +0800

    feat(user): 添加手机号唯一约束并修改相关属性
    - 在 User 实体类中，为 phone 属性添加了唯一约束（unique: true）
    - 将 phone 属性的类型从 number 修改为 string，以更好地处理手机号码
    - 更新了文件的最后修改时间

--- src/modules/user/entity/contacts.ts
+++ src/modules/user/entity/contacts.ts
@@ -2,7 +2,7 @@
  * @Author: 17691002584 17691002584@163.com
  * @Date: 2025-07-23 18:40:16
  * @LastEditors: 17691002584 17691002584@163.com
- * @LastEditTime: 2025-07-23 19:15:43
+ * @LastEditTime: 2025-07-23 19:48:24
  * @FilePath: src/modules/user/entity/contacts.ts
  * @Description: 这是默认设置,可以在设置》工具》File Description中进行配置
  */
@@ -18,6 +18,6 @@ export class ContactEntity extends BaseEntity {
   @Column({ comment: '联系人姓名', nullable: true, length: 50 })
   name: string;
 
-  @Column({ comment: '手机号', nullable: true })
-  phone: number;
+  @Column({ comment: '手机号', nullable: true, unique: true })
+  phone: string;
 }
