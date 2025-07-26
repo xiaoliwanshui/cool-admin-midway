06b46e329bc0c3f492936e7eeba976372f1e141d
17691002584 <17691002584@163.com>
2025-07-26 12:05:49 +0800

    feat(video): 添加轮播图分类过滤功能
    - 在轮播图页面添加分类过滤组件
    - 实现根据分类筛选轮播图数据
    - 优化轮播图数据获取逻辑，支持成人内容过滤

--- src/modules/dict/controller/app/info.ts
+++ src/modules/dict/controller/app/info.ts
@@ -1,13 +1,7 @@
-import { Body, Get, Inject, Post, Provide } from '@midwayjs/core';
-import {
-  BaseController,
-  CoolController,
-  CoolTag,
-  CoolUrlTag,
-  TagTypes,
-} from '@cool-midway/core';
-import { DictInfoService } from '../../service/info';
-import { DictInfoEntity } from '../../entity/info';
+import {Body, Get, Inject, Post, Provide} from '@midwayjs/core';
+import {BaseController, CoolController, CoolTag, CoolUrlTag, TagTypes,} from '@cool-midway/core';
+import {DictInfoService} from '../../service/info';
+import {DictInfoEntity} from '../../entity/info';
 
 /**
  * 字典信息
@@ -24,9 +18,9 @@ import { DictInfoEntity } from '../../entity/info';
       createTime: 'ASC',
     },
     where: ctx => {
-      const { aldult } = ctx.request.headers;
+      const {aldult} = ctx.request.headers;
       if (aldult === '0') {
-        return [['id != :id', { id: 643 }]];
+        return [['id != :id', {id: 643}]];
       }
       return [[]];
     },
@@ -41,14 +35,23 @@ export class AppDictInfoController extends BaseController {
   @Inject()
   dictInfoService: DictInfoService;
 
+  @Inject()
+  ctx;
+
   @CoolTag(TagTypes.IGNORE_TOKEN)
-  @Post('/data', { summary: '获得字典数据' })
+  @Post('/data', {summary: '获得字典数据'})
   async data(@Body('types') types: string[] = []) {
-    return this.ok(await this.dictInfoService.data(types));
+    const data: any = await this.dictInfoService.data(types);
+    const {aldult} = this.ctx.request.headers;
+    if (aldult === undefined || aldult === '0') {
+      console.log('aldult===>', aldult);
+      data.video_category = data.video_category.filter(e => e.id != 643);
+    }
+    return this.ok(data);
   }
 
   @CoolTag(TagTypes.IGNORE_TOKEN)
-  @Get('/types', { summary: '获得所有字典类型' })
+  @Get('/types', {summary: '获得所有字典类型'})
   async types() {
     return this.ok(await this.dictInfoService.types());
   }
