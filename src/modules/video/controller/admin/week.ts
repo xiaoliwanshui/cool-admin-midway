08931bb32e42ab2b0ff4bebd3d435100ba91cb4e
17691002584 <17691002584@163.com>
2025-08-09 01:24:02 +0800

    feat(video): 重构周更新模块
    - 优化了周更新实体结构，移除了不必要的字段
    - 重新设计了周更新的 CRUD 功能，简化了操作流程- 添加了新的 WeekFrom 组件用于选择视频
    - 调整了表格列的显示内容，增加了影片标题和分类等信息- 移除了不必要的弹窗和复杂逻辑

--- src/modules/video/controller/admin/week.ts
+++ src/modules/video/controller/admin/week.ts
@@ -1,5 +1,6 @@
 import { BaseController, CoolController } from '@cool-midway/core';
 import { WeekEntity } from '../../entity/week';
+import { VideoEntity } from '../../entity/videos';
 
 /**
  *
@@ -10,14 +11,66 @@ import { WeekEntity } from '../../entity/week';
   insertParam: ctx => {
     return {
       // 获得当前登录的后台用户ID，需要请求头传Authorization参数
-      createUserId: ctx.admin.userId,
+      createUserId: ctx.admin.userId
     };
   },
   pageQueryOp: {
     fieldEq: ['week'],
-    addOrderBy: {
-      sort: 'desc',
-    },
-  },
+    select: [
+      'a.*',
+      'b.title',
+      'b.category_id',
+      'b.category_pid',
+      'b.surface_plot',
+      'b.cycle',
+      'b.cycle_img',
+      'b.directors',
+      'b.actors',
+      'b.imdb_score',
+      'b.imdb_score_id',
+      'b.douban_score',
+      'b.douban_score_id',
+      'b.introduce',
+      'b.popularity',
+      'b.popularity_day',
+      'b.popularity_week',
+      'b.popularity_month',
+      'b.popularity_sum',
+      'b.note',
+      'b.year',
+      'b.status',
+      'b.duration',
+      'b.region',
+      'b.language',
+      'b.number',
+      'b.total',
+      'b.horizontal_poster',
+      'b.vertical_poster',
+      'b.publish',
+      'b.pubdate',
+      'b.serial_number',
+      'b.screenshot',
+      'b.end',
+      'b.unit',
+      'b.play_url',
+      'b.play_url_put_in',
+      'b.collection_id',
+      'b.up',
+      'b.down',
+      'b.collection_name',
+      'b.sub_title',
+      'b.video_tag',
+      'b.video_class'
+    ],
+    join: [
+      {
+        entity: VideoEntity,
+        alias: 'b',
+        condition: 'a.videoId = b.id',
+        type: 'innerJoin'
+      }
+    ]
+  }
 })
-export class AdminWeekController extends BaseController {}
+export class AdminWeekController extends BaseController {
+}
