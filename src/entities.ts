1d7ef61c308d5390bab5f84820e90a630b529858
17691002584 <17691002584@163.com>
2025-11-20 22:19:46 +0800

    feat(entities): 更新实体导入并添加新模块导出- 在 src/entities.ts 中重新排序并新增 member 模块相关实体导入- 添加 entity19 至 entities 数组中以支持新的分享实体 - 扩展导出列表以包含 member、dict、base 和 application 等模块的新实体- 新增多个控制器和服务组件的导出支持 - 为 video 模块增加新的视频规则实体及相关控制器服务导

--- src/entities.ts
+++ src/entities.ts
@@ -1,23 +1,24 @@
 // 自动生成的文件，请勿手动修改
 import * as entity0 from './modules/video/entity/week_video';
 import * as entity1 from './modules/video/entity/week';
-import * as entity2 from './modules/video/entity/video_line';
-import * as entity3 from './modules/video/entity/video_album_relationship';
-import * as entity4 from './modules/video/entity/videos';
-import * as entity5 from './modules/video/entity/swiper';
-import * as entity6 from './modules/video/entity/play_line';
-import * as entity7 from './modules/video/entity/player';
-import * as entity8 from './modules/video/entity/live';
-import * as entity9 from './modules/video/entity/hot_keyword';
-import * as entity10 from './modules/video/entity/collection_task';
-import * as entity11 from './modules/video/entity/collection_category';
-import * as entity12 from './modules/video/entity/collection';
-import * as entity13 from './modules/video/entity/category';
-import * as entity14 from './modules/video/entity/barrage';
-import * as entity15 from './modules/video/entity/album';
-import * as entity16 from './modules/user/entity/wx';
-import * as entity17 from './modules/user/entity/views';
-import * as entity18 from './modules/user/entity/share';
+import * as entity2 from './modules/video/entity/video_rules';
+import * as entity3 from './modules/video/entity/video_line';
+import * as entity4 from './modules/video/entity/video_album_relationship';
+import * as entity5 from './modules/video/entity/videos';
+import * as entity6 from './modules/video/entity/swiper';
+import * as entity7 from './modules/video/entity/play_line';
+import * as entity8 from './modules/video/entity/player';
+import * as entity9 from './modules/video/entity/live';
+import * as entity10 from './modules/video/entity/hot_keyword';
+import * as entity11 from './modules/video/entity/collection_task';
+import * as entity12 from './modules/video/entity/collection_category';
+import * as entity13 from './modules/video/entity/collection';
+import * as entity14 from './modules/video/entity/category';
+import * as entity15 from './modules/video/entity/barrage';
+import * as entity16 from './modules/video/entity/album';
+import * as entity17 from './modules/user/entity/wx';
+import * as entity18 from './modules/user/entity/views';
+import * as entity19 from './modules/user/entity/share';
 import * as entity20 from './modules/user/entity/like';
 import * as entity21 from './modules/user/entity/info';
 import * as entity22 from './modules/user/entity/contacts';
@@ -29,23 +30,27 @@ import * as entity27 from './modules/space/entity/type';
 import * as entity28 from './modules/space/entity/info';
 import * as entity29 from './modules/recycle/entity/data';
 import * as entity30 from './modules/plugin/entity/info';
-import * as entity31 from './modules/dict/entity/type';
-import * as entity32 from './modules/dict/entity/info';
-import * as entity33 from './modules/demo/entity/goods';
-import * as entity34 from './modules/base/entity/base';
-import * as entity35 from './modules/base/entity/sys/user_role';
-import * as entity36 from './modules/base/entity/sys/user';
-import * as entity37 from './modules/base/entity/sys/role_menu';
-import * as entity38 from './modules/base/entity/sys/role_department';
-import * as entity39 from './modules/base/entity/sys/role';
-import * as entity40 from './modules/base/entity/sys/param';
-import * as entity41 from './modules/base/entity/sys/menu';
-import * as entity42 from './modules/base/entity/sys/log';
-import * as entity43 from './modules/base/entity/sys/department';
-import * as entity44 from './modules/base/entity/sys/conf';
-import * as entity45 from './modules/application/entity/noticeInfo';
-import * as entity46 from './modules/application/entity/feedbackInfo';
-import * as entity47 from './modules/application/entity/ads';
+import * as entity31 from './modules/member/entity/score';
+import * as entity32 from './modules/member/entity/monthlyCheckinConfig';
+import * as entity33 from './modules/member/entity/memberExchangeConfig';
+import * as entity34 from './modules/member/entity/member';
+import * as entity35 from './modules/dict/entity/type';
+import * as entity36 from './modules/dict/entity/info';
+import * as entity37 from './modules/demo/entity/goods';
+import * as entity38 from './modules/base/entity/base';
+import * as entity39 from './modules/base/entity/sys/user_role';
+import * as entity40 from './modules/base/entity/sys/user';
+import * as entity41 from './modules/base/entity/sys/role_menu';
+import * as entity42 from './modules/base/entity/sys/role_department';
+import * as entity43 from './modules/base/entity/sys/role';
+import * as entity44 from './modules/base/entity/sys/param';
+import * as entity45 from './modules/base/entity/sys/menu';
+import * as entity46 from './modules/base/entity/sys/log';
+import * as entity47 from './modules/base/entity/sys/department';
+import * as entity48 from './modules/base/entity/sys/conf';
+import * as entity49 from './modules/application/entity/noticeInfo';
+import * as entity50 from './modules/application/entity/feedbackInfo';
+import * as entity51 from './modules/application/entity/ads';
 export const entities = [
   ...Object.values(entity0),
   ...Object.values(entity1),
@@ -66,6 +71,7 @@ export const entities = [
   ...Object.values(entity16),
   ...Object.values(entity17),
   ...Object.values(entity18),
+  ...Object.values(entity19),
   ...Object.values(entity20),
   ...Object.values(entity21),
   ...Object.values(entity22),
@@ -94,4 +100,8 @@ export const entities = [
   ...Object.values(entity45),
   ...Object.values(entity46),
   ...Object.values(entity47),
+  ...Object.values(entity48),
+  ...Object.values(entity49),
+  ...Object.values(entity50),
+  ...Object.values(entity51),
 ];
